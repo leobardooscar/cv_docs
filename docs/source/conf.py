96c8083fcf9d5c6a0b2eaada4890a59dad4a5517
Leobardo Oscar Alcántara Ocaña <leobardo@iteso.mx>
2024-08-07 07:23:41 -0600

    Furo theme config

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -21,9 +21,7 @@ extensions = [
     'myst_parser',
 ]
 
-extensions = [
-    'sphinx_toolbox.collapse',
-    ]
+
 
 templates_path = ['_templates']
 exclude_patterns = []
@@ -33,7 +31,8 @@ exclude_patterns = []
 # -- Options for HTML output -------------------------------------------------
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#options-for-html-output
 
-html_theme = 'sphinx_rtd_theme'
+#html_theme = 'sphinx_rtd_theme'
+html_theme = 'furo'
 html_static_path = ['_static']
 #Sidebar title
 html_title = "Main page"
@@ -41,3 +40,39 @@ html_title = "Main page"
 html_theme_options = {
     "announcement": "<h2> CV & Project Gallery</h2>  <h3> Leobardo Oscar Alcántara Ocaña</h3> ",
 }
+
+#From Pufflib project
+
+text = '#f1f1f1'
+background = '#061a1a'
+foreground = '#000000'
+highlight = '#00bbbb'
+muted = '#005050'
+
+
+html_theme_options = {
+    "light_css_variables": {
+        "color-foreground-primary": "black",
+        "color-foreground-secondary": muted,
+        "color-foreground-muted": muted,
+        "color-foreground-border": "#878787",
+        "color-background-primary": "white",
+        "color-background-secondary": "#bbcccc",
+        "color-background-hover": "#efeff4ff",
+        "color-background-hover--transparent": "#efeff400",
+        "color-background-border": muted,
+        "color-background-item": "#ccc",
+        "color-announcement-background": "#000000dd",
+        "color-announcement-text": "#eeebee",
+        "color-brand-primary": "black",
+        "color-brand-content": "black",
+        "color-inline-code-background": "#f8f9fb",
+        "color-highlighted-background": "#ddeeff",
+        "color-guilabel-background": "#ddeeff80",
+        "color-guilabel-border": "#bedaf580",
+        "color-card-background": "#bbcccc",
+    },
+    
+   
+}
+
